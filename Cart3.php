464cdc418658c769d63b1620493ab760a889e71a
travistrotto <54992930+travistrotto@users.noreply.github.com>
2020-05-08 23:46:51 -0400

    Shopping Cart

---
+++ Cart3.php
@@ -0,0 +1,210 @@
+<?php
+/*      CSC226 FINAL PROJECT, Prof. Minh Doan
+ *      Spring 2020 
+ *      
+ *      SHOPPING CART USER INTERFACE
+ *      @Author Travis Trotto, Michele,Esmond
+ *      
+ *      # ACCESS'S "Product_details" TABLE IN myphpadmin DATABASE
+ *
+*/
+    session_start();
+    $database_name = "Product_details";
+    $connect = mysqli_connect("localhost","root","",$database_name);
+
+    if (isset($_POST["add"])){
+        if (isset($_SESSION["cart"])){
+            $item_array_id = array_column($_SESSION["cart"],"product_id");
+            if (!in_array($_GET["id"],$item_array_id)){
+                $count = count($_SESSION["cart"]);
+                $item_array = array(
+                    'product_id' => $_GET["id"],
+                    'item_name' => $_POST["hidden_name"],
+                    'product_price' => $_POST["hidden_price"],
+                    'item_quantity' => $_POST["quantity"],
+                );
+                $_SESSION["cart"][$count] = $item_array;
+                echo '<script>window.location="Cart2.php"</script>';
+            }else{
+                echo '<script>alert("That product is already in your cart, remove it to change or edit the quantity")</script>';
+                echo '<script>window.location="Cart2.php"</script>';
+            }
+        }else{
+            $item_array = array(
+                'product_id' => $_GET["id"],
+                'item_name' => $_POST["hidden_name"],
+                'product_price' => $_POST["hidden_price"],
+                'item_quantity' => $_POST["quantity"],
+            );
+            $_SESSION["cart"][0] = $item_array;
+        }
+    }
+
+    if (isset($_GET["action"])){
+        if ($_GET["action"] == "delete"){
+            foreach ($_SESSION["cart"] as $keys => $value){
+                if ($value["product_id"] == $_GET["id"]){
+                    unset($_SESSION["cart"][$keys]);
+                    echo '<script>alert("Product removed from cart successfully")</script>';
+                    echo '<script>window.location="Cart2.php"</script>';
+                }
+            }
+        }
+    }
+?>
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>K-Daysi Shopping Cart</title>
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <style>
+        @import url('https://fonts.googleapis.com/css?family=Titillium+Web');
+
+        *{
+           
+        }
+        .product{
+            border: 1px solid #eaeaec;
+            margin: -1px 19px 3px -1px;
+            padding: 10px;
+            text-align: center;
+            background-color: #efefef;
+        }
+        table, th, tr{
+            text-align: center;
+        }
+        .title2{
+            text-align: center;
+            color: white;
+            background-color: purple;
+            padding: 2%;
+        }
+        h2{
+            text-align: center;
+            color: white;
+            background-color: purple;
+            padding: 2%;
+        }
+        table th{
+            background-color: #efefef;
+        }
+    </style>
+</head>
+<body>
+
+
+    <div class="container" style="width: 65%">
+        <a href="Cart2.php"><img src="logo.jpg"></a>
+        <!--
+    <div class="sCart">
+        <h2>Available Products</h2>
+        <p style="text-align: center;"><strong>These are the products sold by K-Daysi.</strong></p>
+         <p style="text-align: center;">Each product you see listed below is in the K-Daysi database of available products. <br>The name, price and image of each product is passed from the database into the HTML and CSS you see here.</p>
+        <?php
+            $query = "SELECT * FROM product ORDER BY id ASC ";
+            $result = mysqli_query($connect,$query);
+            if(mysqli_num_rows($result) > 0) {
+
+                while ($row = mysqli_fetch_array($result)) {
+
+                    ?>
+                    <div class="col-md-3">
+
+                        <form method="post" action="Cart2.php?action=add&id=<?php echo $row["id"]; ?>">
+
+                            <div class="product">
+                                <img src="<?php echo $row["image"]; ?>" class="img-responsive">
+                                <h5 class="text-info"><?php echo $row["pname"]; ?></h5>
+                                <h5 class="text-danger"><?php echo $row["price"]; ?></h5>
+                                <input type="text" name="quantity" class="form-control" value="1">
+                                <input type="hidden" name="hidden_name" value="<?php echo $row["pname"]; ?>">
+                                <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>">
+                                <input type="submit" name="add" style="margin-top: 5px;" class="btn btn-success"
+                                       value="Add to Cart">
+                            </div>
+                        </form>
+                    </div>
+                    <?php
+                }
+            }
+        ?>
+    </div>
+            -->
+        <div style="clear: both"></div>
+
+     <div class="sCart">
+        <h3 class="title2">Your Shopping Cart</h3>
+        <p style="text-align: center;"><strong>These are the products listed in your shopping cart.</strong></p>
+         <p style="text-align: center;">Each product you see listed below is selected by you from the above products.<br>A PHP file keeps track of your "purchases" and lists the items and totals in the table below.</p>
+
+        <div class="table-responsive">
+            <table class="table table-bordered">
+            <tr>
+                <th width="30%">Product Name</th>
+                <th width="10%">Quantity</th>
+                <th width="13%">Price Details</th>
+                <th width="10%">Total Price</th>
+                <th width="17%">Remove Item</th>
+            </tr>
+
+            <?php
+                if(!empty($_SESSION["cart"])){
+                    $total = 0;
+                    foreach ($_SESSION["cart"] as $key => $value) {
+                        ?>
+                        <tr>
+                            <td><?php echo $value["item_name"]; ?></td>
+                            <td><?php echo $value["item_quantity"]; ?></td>
+                            <td>$ <?php echo $value["product_price"]; ?></td>
+                            <td>
+                                $ <?php echo number_format($value["item_quantity"] * $value["product_price"], 2); ?></td>
+                            <td><a href="Cart2.php?action=delete&id=<?php echo $value["product_id"]; ?>"><span
+                                        class="text-danger">Remove Item</span></a></td>
+
+                        </tr>
+                        <?php
+                        $total = $total + ($value["item_quantity"] * $value["product_price"]);
+                    }
+                        ?>
+                        <tr>
+                            <td colspan="3" align="right">Total</td>
+                            <th align="right">$ <?php echo number_format($total, 2); ?></th>
+                            <td></td>
+                        </tr>
+                        <?php
+                    }
+                ?>
+                <div class="productsBtn">
+                <a href="Cart2.php">
+                    <button class="btn btn-success">
+                    View All Products
+                    </button>
+                </a>
+             
+
+                <a href="Cart2.php">
+                    <button class="btn">
+                    View My Account
+                    </button>
+                </a>
+             
+            </div>
+
+            </table>
+        </div>
+
+    </div>
+         <a href="Cart3.php"><p style="text-align: center;">Click here to view all products</p></a>
+    </div>
+
+
+</body>
+</html>
